bee29687cfd82c2dd14d56b0ffde1a2b9e8a8032
IcaroAguiar <icaroaguiar14@gmail.com>
2022-08-30 16:00:34 -0300

    'BlackList'

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,22 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Login } from './login.model';
+import { CadastroService } from '../cadastro/cadastro-pag/cadastro.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
+  login!: Login;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private loginService: CadastroService) {}
 
-  ngOnInit(): void {
-  }
-  
+  ngOnInit(): void {}
+
   cadastrar() {
-    
-    this.router.navigate(['/login/cadastro'])
+    this.router.navigate(['/login/cadastro']);
+  }
+  onSubmit() {
+    const blackList: string[] = [
+      'select ',
+      'from ',
+      'or ',
+      'drop ',
+      'having ',
+      'insert ',
+      'group ',
+      'exec ',
+      "'",
+      '"',
+      '--',
+      '#',
+    ];
 
+    blackList.forEach((palavra) => {
+      if (this.login.usuario.toLowerCase().includes(palavra)) {
+        throw (
+          (new Error('Dados inválidos! Há palavras suspeitas nesse formulário'),
+          this.loginService.showMessage('Dados invalidos'))
+        );
+      }
+    });
   }
 }
